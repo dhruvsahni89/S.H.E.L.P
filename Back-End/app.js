cec05f353006fb8dcf64881a7fd8202a535e0893
ayush-lab <ayush.millionaire8750@gmail.com>
2020-09-22 18:43:38 +0530

    removed CORS error and changed port

--- Back-End/app.js
+++ Back-End/app.js
@@ -7,14 +7,14 @@ const signupRoute = require('./routes/signup'); //importing signup route
 const errorController = require('./controllers/error')
 
 const app = express();
-const port = 3000 //whatever is in the environment variable PORT or 3000
+const port =8080; //whatever is in the environment variable PORT or 3000
 
 app.use(bodyParser.json()); // For parsing the incoming json file from the client
 
 app.use((req, res, next) =>{  // To remove CROS (cross-resource-origin-platform) problem 
     res.setHeader('Allow-Control-Allow-Origin','*'); // to allow all client we use *
     res.setHeader('Allow-Control-Allow-Methods','OPTIONS,GET,POST,PUT,PATCH,DELETE'); //these are the allowed methods 
-    res.setHeader('Allow-Control-Allow-Headers','Content-Type,Authorization'); // allowed headers (Auth for extra data related to authoriaztiom)
+    res.setHeader('Access-Control-Allow-Headers', "*"); // allowed headers (Auth for extra data related to authoriaztiom)
     next();
 })
 
