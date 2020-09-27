3daf613baac7129bf2fe1b43f1c42c649e430e2f
Dhruv Sahni <dhruvsahni89@gmail.com>
2020-09-28 01:58:32 +0530

    merging

---
+++ Back-End/middleware/is-auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const token = req.get("Authorization").split(" ")[1];
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, 'supersecret');
+      } catch (err) {
+        err.statusCode = 500;
+        throw err;
+      }
+      if (!decodedToken) {
+        const error = new Error("Not authenticated.");
+        error.statusCode = 401;
+        throw error;
+      }
+      req.userId = decodedToken.userId;
+      next();
+};
