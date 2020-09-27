c7ecf29fffffcd36240d832522b591790126f252
Dhruv Sahni <dhruvsahni89@gmail.com>
2020-09-28 02:12:50 +0530

    added middleware to check authorized users

--- Back-End/routes/creator.js
+++ Back-End/routes/creator.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { body } =  require('express-validator/check'); // for validation of data
 const Courses = require('../models/courses');
 const courseController = require('../controllers/courses');
+const isAuth=require('../middleware/is-auth');
 const router = express.Router();
 
 
 router.post('/creator/create-course',[
     body('title').trim().isLength({min:5}),
     body('name').trim().isLength({min:5}) 
-],courseController.createcourse);
+],isAuth,courseController.createcourse);
 
 
 module.exports = router;
