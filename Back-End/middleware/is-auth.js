a1ceeb3665061df85e839a3847ce7dc0203d227f
Dhruv Sahni <dhruvsahni89@gmail.com>
2020-09-28 02:11:37 +0530

    added middleware to check authorized users

--- Back-End/middleware/is-auth.js
+++ Back-End/middleware/is-auth.js
@@ -1,7 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const token = req.get("Authorization").split(" ")[1];
+    const authHeader = req.get("Authorization");
+    if (!authHeader) {
+      const error = new Error("Not authenticated.");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = authHeader .split(" ")[1];
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, 'supersecret');
