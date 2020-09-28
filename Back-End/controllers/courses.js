08f5653e766c6b5d154245209c9a0d69e566b4c2
Abhishek Srivas <abhiautar123@gmail.com>
2020-09-28 10:33:03 +0530

    Course addtion with user _id in DB

--- Back-End/controllers/courses.js
+++ Back-End/controllers/courses.js
@@ -1,6 +1,6 @@
 const {validationResult} = require('express-validator/check');
 const courses = require('../models/courses');
-//const users = require('../models/users');
+const Users = require('../models/users');
 
 exports.createcourse = (req, res, next) => {
     console.log("hello");
@@ -15,24 +15,29 @@ exports.createcourse = (req, res, next) => {
 //        error.statusCode = 422;
 //        throw error;
 //    }
+    const email = req.body.email;
     const title = req.body.title;
     const imageUrl = req.file.path;
     const name = req.body.name;
     const discription = req.body.discription;
     console.log(imageUrl);
+    const userID = req.body._id;
+
     const course = new courses({
         title: title,
-       name: name,
-      discription: discription,
-        imageurl: imageUrl
-    })
+        name: name,
+        discription: discription,
+        imageurl: imageUrl,
+        creator: userID
+    });
+
     course.save().then(result =>{
         console.log(result);
         res.status(201).json({message:'Course Created',post: result})
     }).catch(err => {
         console.log(err);
     });
-};
 
+}
 
 
